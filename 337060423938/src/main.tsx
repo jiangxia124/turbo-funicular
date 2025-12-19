1958665fffd3520f235b29266ff8b84acf5538a7
jiangxia124 <3055442557@qq.com>
2025-12-19 12:18:33 +0800

    Configure basename for GitHub Pages
    Set the basename for GitHub Pages routing.

--- 337060423938/src/main.tsx
+++ 337060423938/src/main.tsx
@@ -5,6 +5,12 @@ import { Toaster } from 'sonner';
 import App from "./App.tsx";
 import "./index.css";
 
+
+// 为GitHub Pages设置正确的基础路径
+// 如果您的仓库URL是 https://username.github.io/repo-name/
+// 则需要将basename设置为 "/repo-name"
+// 当前设置为空字符串，表示使用根路径
+const basename = "jiangxia124/turbo-funicular/edit/jiangxia124-patch-3/337060423938/";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
